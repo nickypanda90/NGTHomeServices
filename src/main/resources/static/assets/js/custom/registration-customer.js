b473f351ad1ca1e969caa91ba3f7b85c1dffd28a
tjain16 <jain.tripti6@gmail.com>
2020-04-05 22:52:24 -0500

    Adding changes for customer registration

--- src/main/resources/static/assets/js/custom/registration-customer.js
+++ src/main/resources/static/assets/js/custom/registration-customer.js
@@ -1,18 +1,30 @@
- $(document).ready(function () {
+$(document).ready(function () {
     
 });
 
+   function getFormData($form){
+     var unindexed_array = $form.serializeArray();
+     var indexed_array = {};
+
+     $.map(unindexed_array, function(n, i){
+       indexed_array['"'+n['name']+'"'] = n['value'];
+     });
+
+     return indexed_array;
+   }
+
 function customerRegistration(){
+  var $form = $("#customer_form");
+  var data = getFormData($form);
+  console.log(data);
   let customer_form = $("#customer_form");
   let post_url = customer_form.attr("action"); //get form action url
   let request_method = customer_form.attr("method"); //get form GET/POST method
-  console.log(request_method);
-  let form_data = customer_form.serialize();
-  console.log(form_data);
+
   $.ajax({
     url : post_url,
     type: request_method,
-    data : form_data,
+    data : JSON.stringify(data),
     crossDomain: true,
     contentType: "application/json;",
     dataType: "json",
@@ -20,6 +32,6 @@ function customerRegistration(){
     processData:false
     }).done(function(response){ 
       console.log(response);
-      window.location.replace("./index.html");
+      window.location.replace("../index.html");
   });
 }
