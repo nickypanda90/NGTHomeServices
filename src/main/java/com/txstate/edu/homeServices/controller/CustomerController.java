5031d4cd689f42d954698712bdd0213268fe4748
nickypanda90 <60499714+nickypanda90@users.noreply.github.com>
2020-04-05 23:34:01 -0500

    commitApril5

--- src/main/java/com/txstate/edu/homeServices/controller/CustomerController.java
+++ src/main/java/com/txstate/edu/homeServices/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.txstate.edu.homeServices.controller;
 
 
+import com.txstate.edu.homeServices.model.CustomerLogin;
 import com.txstate.edu.homeServices.model.CustomerRegistration;
+import com.txstate.edu.homeServices.repository.AuthenticRepository;
 import com.txstate.edu.homeServices.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +16,9 @@ public class CustomerController {
     @Autowired
     CustomerRepository customerRepository;
 
-//    @GetMapping("/users")
-//    public List<CustomerRegistration> getAllUsers() {
-//        return customerRepository.findAll();
-//    }
+    @Autowired
+    AuthenticRepository authenticRepository;
 
-//    @GetMapping("/users/{id}")
-//    public CustomerRegistration getUserById(@PathVariable(value = "id") Integer userId) {
-//        return customerRepository.findById(userId).orElseThrow(() -> new NullPointerException("test"));
-//    }
 
     @PostMapping("/signup")
     public CustomerRegistration signupCustomer(@Valid @RequestBody CustomerRegistration customerregistration) {
@@ -38,6 +34,27 @@ public class CustomerController {
         }
         return temp;
     }
+
+    @PostMapping("/login")
+    public boolean signupCustomer(@Valid @RequestBody CustomerLogin customerlogin) {
+        if(customerRepository.authenticate(customerlogin.getEmail_id(),customerlogin.getPassword()).size()>0)
+            return true;
+        else
+            return false;
+
+    }
+
+    @PostMapping("/authenticateemailid")
+   public boolean authenticEmailid(@Valid @RequestBody CustomerRegistration customerregistration) {
+            if(authenticRepository.authenticate(customerregistration.getEmail_id()).size()>0)
+
+           return true;
+
+       else
+            return false;
+
+  }
+
 
 
 }
--- src/main/java/com/txstate/edu/homeServices/repository/CustomerRepository.java
+++ src/main/java/com/txstate/edu/homeServices/repository/CustomerRepository.java
@@ -1,9 +1,14 @@
 package com.txstate.edu.homeServices.repository;
 import com.txstate.edu.homeServices.model.CustomerRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerRegistration, Integer> {
 
+    @Query("SELECT c FROM CustomerRegistration c WHERE LOWER(c.email_id) = LOWER(:email_id) and c.password = :password")
+    public List<CustomerRegistration> authenticate(String email_id, String password);
 }
