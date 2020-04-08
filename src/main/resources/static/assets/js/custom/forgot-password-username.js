e30e4804855818ef21b3ec7d95bc66a690d93730
tjain16 <jain.tripti6@gmail.com>
2020-04-08 00:34:50 -0500

    Update the forgot username and its styles

---
+++ src/main/resources/static/assets/js/custom/forgot-password-username.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('.alert-danger').hide();
+});
+
+
+function goToPassword() {
+    var $form = $("#forgot-username");
+    let post_url = $form.attr("action");
+    let request_method = $form.attr("method");
+    let data = getFormData($form);
+
+    $.ajax({
+        url : post_url,
+        type: request_method,
+        data : JSON.stringify(data),
+        crossDomain: true,
+        contentType: "application/json;",
+        dataType: "json",
+        cache: false,
+        processData:false
+        }).done(function(response){ 
+            console.log(response);
+            if(response) {
+                window.location.replace("./forgot-password.html");
+            } else {
+                // update error message                
+                $form.find('.alert-danger').show();
+            }
+            
+      });
+}
+
+
+function getFormData($form){
+    let unindexed_array = $form.serializeArray();
+    let indexed_array = {};
+  
+    $.map(unindexed_array, function(n, i){
+      indexed_array[n['name']] = n['value'];
+    });
+  
+    return indexed_array;
+  }
