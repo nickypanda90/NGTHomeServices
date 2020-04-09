7be11cea803d8dc434a9af40e247622ec115b668
sangree <36014387+sangree@users.noreply.github.com>
2020-04-09 10:23:44 -0500

    added business registration.

--- src/main/java/com/txstate/edu/homeServices/repository/BusinessRepository.java
+++ src/main/java/com/txstate/edu/homeServices/repository/BusinessRepository.java
@@ -1,9 +1,10 @@
 package com.txstate.edu.homeServices.repository;
 
-import com.txstate.edu.homeServices.model.Business;
+import com.txstate.edu.homeServices.model.BusinessEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BusinessRepository extends JpaRepository<Business, Integer> {
+public interface BusinessRepository extends JpaRepository<BusinessEntity, Integer> {
+
 }
--- src/main/java/com/txstate/edu/homeServices/service/BusinessService.java
+++ src/main/java/com/txstate/edu/homeServices/service/BusinessService.java
@@ -1,6 +1,7 @@
 package com.txstate.edu.homeServices.service;
 
-import com.txstate.edu.homeServices.model.Business;
+import com.txstate.edu.homeServices.model.BusinessEntity;
+import com.txstate.edu.homeServices.model.BusinessRegistration;
 import com.txstate.edu.homeServices.repository.BusinessRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,20 @@ public class BusinessService {
     @Autowired
     private BusinessRepository repository;
 
-    public List<Business> getAllBusinesses() {
+    public List<BusinessEntity> getAllBusinesses() {
         return repository.findAll();
+    }
+
+    public BusinessRegistration register(BusinessRegistration registration) {
+        BusinessEntity entity = new BusinessEntity();
+        entity.setName(registration.getName());
+        entity.setAddress(registration.getAddress());
+        entity.setEmail(registration.getEmail());
+        entity.setPassword(registration.getPassword());
+        entity.setCategory(registration.getCategory());
+        BusinessEntity savedEntity = repository.save(entity);
+
+        registration.setId(savedEntity.getBusinessId());
+        return registration;
     }
 }
