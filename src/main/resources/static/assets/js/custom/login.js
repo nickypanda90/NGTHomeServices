645439aafe774c8c6b5db45c7b092dde89b3edb1
tjain16 <jain.tripti16@gmail.com>
2020-04-11 13:47:37 -0500

    Adding cateogory to contractor registration and some validations

--- src/main/resources/static/assets/js/custom/login.js
+++ src/main/resources/static/assets/js/custom/login.js
@@ -21,12 +21,12 @@ function submitLoginForm() {
 
 
 function getFormData($form){
-    let unindexed_array = $form.serializeArray();
-    let indexed_array = {};
-  
-    $.map(unindexed_array, function(n, i){
-      indexed_array[n['name']] = n['value'];
-    });
-  
-    return indexed_array;
-  }
+  let unindexed_array = $form.serializeArray();
+  let indexed_array = {};
+
+  $.map(unindexed_array, function(n, i){
+    indexed_array[n['name']] = n['value'];
+  });
+
+  return indexed_array;
+}
