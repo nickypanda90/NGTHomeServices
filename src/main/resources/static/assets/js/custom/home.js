8d05c71f3d2fd57c9510cc490f495e66b59549de
tjain16 <jain.tripti16@gmail.com>
2020-04-20 13:08:03 -0500

    Adding changes for find service typeahead

--- src/main/resources/static/assets/js/custom/home.js
+++ src/main/resources/static/assets/js/custom/home.js
@@ -41,3 +41,50 @@ $(document).ready(function () {
     });
     return vars;
   }
+
+// Typeahead for services
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var services = ["carpetCleaning", "homeCleaning","painting",
+        "plumbing","gutterCleaning","masonry",
+        "roofing","windows","fencing","handymen",
+        "landscaping","pestControl"
+];
+
+$('.typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'services',
+  source: substringMatcher(services)
+});
+
+
+function submitService(){
+  let find_service_val = $('.tt-input').val();
+  let redirectUrl = "pages/customer/service-page.html?serviceType=" + find_service_val;
+  window.location.replace(redirectUrl); 
+}
