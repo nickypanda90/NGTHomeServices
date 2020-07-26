38f8f6b4bfd56cca5570b97207b3f53645761d5a
tjain16 <jain.tripti16@gmail.com>
2020-07-26 00:58:33 -0500

    Add service history changes for approved status

--- src/main/resources/static/assets/js/custom/service-history.js
+++ src/main/resources/static/assets/js/custom/service-history.js
@@ -60,7 +60,7 @@ function scrollToDownload() {
                       + "<input type='text' id='date-"+ val.serviceId  + "' value ='" + moment(val.serviceDateTime).format("MM-DD-YYYY HH:mm:ss")  + "' name='service_date_time' class='form-control datetimepicker unedit'> <span class='edit' id='spandate-"+ val.serviceId  + "'>" 
                       + (val.serviceDateTime ? moment(val.serviceDateTime).format("MM-DD-YYYY HH:mm:ss")  : "" ) + 
                     "</span></td><td>" + val.serviceCategory + "</td><td id= "+statusId+">"+ (val.status ? val.status : "") +
-                    "</td><td id="+actionID+" >" + ( val.status == "Pending"  ? "<a href class='unedit' onclick='action(this)' title='Approve' data-val='"+ JSON.stringify(val)+ "' ><i class='fa fa-check success' aria-hidden='true'></i></a> <a href class='edit' onclick='action(this)' title='Edit Service' data-val='"+ JSON.stringify(val)+ "' ><i class='fa fa-pencil pencil' aria-hidden='true'></i></a> <a href'' data-val='"+ JSON.stringify(val)+ "' onclick='action(this)' title='Cancel Service'><i class='fa fa-times danger' aria-hidden='true'></i></a>" :  "" )  + "</td>");
+                    "</td><td id="+actionID+" >" + ( val.status == "Pending" || val.status == "Approved" ? "<a href class='unedit' onclick='action(this)' title='Approve' data-val='"+ JSON.stringify(val)+ "' ><i class='fa fa-check success' aria-hidden='true'></i></a> <a href class='edit' onclick='action(this)' title='Edit Service' data-val='"+ JSON.stringify(val)+ "' ><i class='fa fa-pencil pencil' aria-hidden='true'></i></a> <a href'' data-val='"+ JSON.stringify(val)+ "' onclick='action(this)' title='Cancel Service'><i class='fa fa-times danger' aria-hidden='true'></i></a>" :  "" )  + "</td>");
         tbody.append(row);
 
         $('.unedit').hide();
@@ -107,7 +107,9 @@ function scrollToDownload() {
     const rowid = data.serviceId;
     if($(self).find('i').hasClass('pencil')) {
       if(checkDate(data.serviceDateTime)){
+        $('#' + data.status + rowid).html('Pending');
         data.status = "Pending";
+
         $('tr[id='+ rowid +']').find('.unedit').show();
         $('tr[id='+ rowid +']').find('.edit').hide();
       }
@@ -152,7 +154,7 @@ function scrollToDownload() {
       if(response) {
         $('tr[id='+ response.service_id +']').find('.edit').show();
         $('tr[id='+ response.service_id +']').find('.unedit').hide();
-        $("#Pending"+response.service_id).html(response.status);
+        $("#" + response.status +response.service_id).html(response.status);
         //$("#action"+response.service_id).html("");
 
       }   
